9eb6895bcfb8f869dc2badd25f2f12798be9cc48
kentaro.handa <kentaro.handa@gmail.com>
2023-02-16 03:03:46 +0900

    最低限の処理を実装

--- controller/watcher.py
+++ controller/watcher.py
@@ -1,15 +1,18 @@
+import time
+
 import instaloader
 
 from controller.notificator import Notificator
 
-INTERVAL_SEC = 60 * 5
-LIMIT = 10
+INTERVAL_SEC = 60 * 60
+
+KEYWORDS = ["PR", "pr"]
 
 
 class Target(object):
     def __init__(self, user_id):
         self.user_id = user_id
-        self.latest_post_data = None
+        self.last_check_timestamp = time.time()
 
 
 class Watcher(object):
@@ -19,55 +22,48 @@ class Watcher(object):
         self.notificator = Notificator(webhook_url)
         self.targets = [Target(user_id) for user_id in targets]
 
-    # def check(self):
-    #     loader = instaloader.Instaloader()
-    #     loader.login(self.user_id, self.password)
-    #     loader.dirname_pattern = "src/{target}"
-    #
-    #     for target in self.targets:
-    #         posts = instaloader.Profile.from_username(loader.context, target.user_id).get_posts()
-    #         latest_post_data = target.latest_post_data
-    #
-    #         for post in posts:
-    #             if latest_post_data is None or latest_post_data > latest_post_data:
-    #                 target.latest_post_data = post.data
-    #             post.data
-    # if target.latest_post is None:
-    #
-    # caption = post.caption
-    # hashtag = post.caption_hashtags
-
-    def run(self):
+    def check(self):
         loader = instaloader.Instaloader()
         loader.login(self.user_id, self.password)
-        loader.dirname_pattern = "src/{target}"
 
-        posts = instaloader.Profile.from_username(
-            loader.context, self.targets[0].user_id
-        ).get_posts()
-        count = 0
-        for post in posts:
-            print(post.date)
-            print(type(post.date))
+        for target in self.targets:
+            posts = instaloader.Profile.from_username(loader.context, target.user_id).get_posts()
+            profile = instaloader.Profile.from_username(loader.context, target.user_id)
+            counter = 0
+            for post in posts:
+                counter += 1
+                if post.date.timestamp() > target.last_check_timestamp:
+                    caption = post.caption
+                    for keyword in KEYWORDS:
+                        if keyword in caption:
+                            self.send_pr_post(post, profile)
+                            break
 
-            count += 1
-            # loader.download_post(post, self.targets[0])
-            if count > 0:
-                break
+                if counter > 3 and post.date.timestamp() < target.last_check_timestamp:
+                    break
 
-    def send_pr_post(self, user_id, url, caption, icon, image_path):
+            target.last_check_timestamp = time.time()
+
+    def run(self):
+        while True:
+            try:
+                self.check()
+            except Exception as e:
+                print(e)
+
+            time.sleep(INTERVAL_SEC)
+
+    def send_pr_post(self, post, profile):
         self.notificator.notify(
             payload={
                 "username": "ig-pr-times",
                 "attachments": [
                     {
-                        "author_name": f"{user_id}",
-                        "author_link": f"{url}",
-                        "author_icon": f"{icon}",
-                        "title": "PR投稿",
-                        "text": f"{caption}",
-                        "image_url": f"{image_path}",
-                        "footer": "Send from Python",
+                        "author_name": f"{post.owner_username}",
+                        "author_link": f"https://www.instagram.com/{post.owner_username}",
+                        "author_icon": profile.profile_pic_url,
+                        "text": f"{post.caption}",
+                        "image_url": f"{post.url}",
                     }
                 ],
             }
